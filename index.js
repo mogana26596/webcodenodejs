8e0797191beab6b33daa7f306686cc9423706987
mogana26596 <108892214+mogana26596@users.noreply.github.com>
2022-12-16 10:59:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 mongo.connect();
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"*",credentials : true}))
 app.use(express.json());
 
 app.use('/register', register);
